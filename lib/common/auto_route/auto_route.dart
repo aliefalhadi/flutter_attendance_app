41d6c82e907706ddc356a3c7bd62b6356680cf41
Alief Alhadi <alief.alhadi@majoo.id>
2024-01-19 20:32:16 +0700

    init project

---
+++ lib/common/auto_route/auto_route.dart
@@ -0,0 +1,15 @@
+import 'package:auto_route/auto_route.dart';
+
+@AutoRouterConfig(
+  replaceInRouteName: 'Page,Route',
+)
+class AppRouter extends $AppRouter {
+  AppRouter();
+
+  @override
+  RouteType get defaultRouteType => const RouteType.adaptive();
+  @override
+  final List<AutoRoute> routes = [
+    AutoRoute(page: SplashScreenRoute.page, path: '/'),
+  ];
+}
